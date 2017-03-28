0086d7fba93b23c14185dcd4f76e403e7a6ce7d0
Alpha <aqworldjohnsonalpha1@gmail.com>
2017-03-28 16:28:58 -0500

    Added: SoundOnCollision Script
    This script plays a AudioClip when ever a rigidbody collides.

---
+++ CVediaPong/Assets/Scripts/SoundOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOnCollision : MonoBehaviour {
+
+    // Sound to be played
+    public AudioClip Clip;
+
+    // Plays a sound with the NGUI.PlaySound helper function
+    void OnCollisionEnter(Collision col)
+    {
+        if (Clip == null)
+            return;
+        NGUITools.PlaySound(Clip);
+    }
+
+}
