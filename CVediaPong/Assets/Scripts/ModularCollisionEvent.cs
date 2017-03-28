fecc8f1e8fd16a60d08dbea87af45eb0f1a8765c
Alpha <aqworldjohnsonalpha1@gmail.com>
2017-03-28 15:57:20 -0500

    Added: Collision Event Class
    A modular class for handling collision events.

---
+++ CVediaPong/Assets/Scripts/ModularCollisionEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A modular class meant to able to be thown on any object with a collider and
+// trigger events on the collision.
+public class ModularCollisionEvent : MonoBehaviour {
+
+    // The Event to be triggered
+    public EventDelegate Event;
+    // GameObject To Expect the collision with.
+    public GameObject ExpectedCollisionObject;
+
+    // This event calls when this gameobject collides with the expected rigidbody then calls the EventDelegate
+    void OnCollisionEnter(Collision col)
+    {
+        if (ExpectedCollisionObject == null)
+            return;
+        if (col.gameObject == ExpectedCollisionObject)
+        {
+            if (Event != null)
+                Event.Execute();
+        }
+    }
+
+}
